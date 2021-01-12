dbd55c807d2068410a1b6bf2507ffca9a5be5959
luckymaojing <3177136519@qq.com>
2021-01-12 18:00:56 +0800

    配置了部分路由，其他的跟着几个差不多就行

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,13 +1,13 @@
-// 例：
 // 引入axios
-let request = require('../utils/request');
+let request = require("../utils/request");
 
+// 例：
 // 验证用户名
 //功能：验证用户名是否存在
 // function checkName(username) {
 //     return request.get('/user/checkname',{
 //         params : {
-//             username 
+//             username
 //         }
 //     })
 // }
--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,22 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "Home",
-  //   component: Home
-  // },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
+  {
+    path: "/",
+    name: "home",
+    component: () => import('../views/Home'),
+    meta: {
+      title: '首页'
+    },
+  },
+  {
+    path: "/reg",
+    name: "reg",
+    component: () => import('../views/Reg'),
+    meta: {
+      title: '登录页'
+    },
+  },
 ];
 
 const router = new VueRouter({
